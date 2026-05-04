e254cca6062c9ba468dbb32550cfd1eade0af757
Jamius Siam <jamius.siam@therapservices.net>
2026-05-04 09:12:39 +0600

    Scaffolded the initial Spring Boot backend and React fronend

---
+++ weaver-backend/src/main/java/com/jamiussiam/weaver/WeaverApplication.java
@@ -0,0 +1,13 @@
+package com.jamiussiam.weaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaverApplication.class, args);
+	}
+
+}
---
+++ weaver-backend/src/test/java/com/jamiussiam/weaver/WeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamiussiam.weaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
