bba142ef90feca68cc129f2de00e91fdda3bc4c7
Jamius Siam <j@sii.am>
2026-05-28 14:59:04 +0600

    Changed styling for ShadCN

--- flightdrift-frontend/src/routes/index.tsx
+++ flightdrift-frontend/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button.tsx";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -8,6 +9,8 @@ function Index() {
   return (
     <div>
       <h3 className="text-4xl">Welcome Home!</h3>
+      <p className="text-muted-text">Test Muted</p>
+      <Button>Click Me</Button>
     </div>
   );
 }
